09fbc489f02c233a4119359fbf912b3c00337293
sohaibya <sohaibasif2019@gmail.com>
2022-04-18 06:22:51 -0700

    added the event listener

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ class Book {
         id += 1;
     }   
 }
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    Title=form.Title.value;
+    Author=form.Author.value;
+    let book =new Book(Title,Author);
+    Books.push(book);
+    booksList.innerHTML += ` <p id="title">${Title}</p>
+    <p id="author">${Author}</p>
+    <button type="submit" class="remove-button" onclick="removeBook(this)>Remove</button>
+    <hr>` 
+    localStorage.Books = JSON.stringify(Books);
+  }
+  
+    
+);
