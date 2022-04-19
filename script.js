33dff8adab977ae704891ad97350aa785db63aaf
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-04-19 07:45:30 +0100

    Update functions

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
+let Books = [];
 
-//Get Html elements
-let form=document.querySelector("#add-books");
-let booksList=document.querySelector(".Books-list");
-let Title=document.querySelector("#titleInput").value;
-let Author=document.querySelector("#authorInput").value;
-let add=document.querySelector(".Add-button");
+// Book Div and Template const
+const temp = document.querySelector('.book');
+const bookshelf = document.querySelector('#bookshelf');
 
-//intialize array
-let Books =[];
-let id = Books.length;
+const submit = document.querySelector('#submit');
+let idBook = Books.length;
 
-//Create Array of books
-class Book {
-    constructor (name,author) {
-        this.name=name;
-        this.author=author;
-        id += 1;
-    }   
+function Book(title, author) {
+  this.id = idBook;
+  this.title = title;
+  this.author = author;
+  idBook += 1;
 }
-form.addEventListener('submit',(e) => {
-    e.preventDefault();
-    Title=form.Title.value;
-    Author=form.Author.value;
-    let book =new Book(Title,Author);
-    Books.push(book);
-    booksList.innerHTML += ` <p id="title">${Title}</p>
-    <p id="author">${Author}</p>
-    <button type="submit" class="remove-button">Remove</button>
-    <hr>` 
-    localStorage.Books = JSON.stringify(Books);
-  } 
-);
 
-ReloadBook();
-
-//Reload function
-function ReloadBook() {
-    Books = JSON.parse(localStorage.Books);
-    for (let i = 0; i < Books.length; i += 1) {
-      // eslint-disable-next-line no-use-before-define
-      booksList.innerHTML += ` <p id="title">${Books[i].name}</p>
-      <p id="author">${Books[i].author}</p>
-      <button type="submit" class="remove-button">Remove</button>
-      <hr>` 
-    }
+function ReloadBooks() {
+  Books = JSON.parse(localStorage.Books);
+  bookshelf.innerHTML = '';
+  bookshelf.appendChild(temp);
+  for (let i = 0; i < Books.length; i += 1) {
+    DisplayBook(Books[i]);
   }
- 
-  ReloadBook() 
+}
+
+function DeleteBook(id) {
+  Books = Books.filter((book) => book.id !== id);
+  localStorage.Books = JSON.stringify(Books);
+  ReloadBooks();
+}
+
+function DisplayBook(book) {
+  const BookList = temp.content.cloneNode(true);
+  BookList.querySelectorAll('h2')[0].innerHTML = book.title;
+  BookList.querySelectorAll('h2')[1].innerHTML = book.author;
+  BookList.querySelector('button').addEventListener('click', () => { DeleteBook(book.id); });
+  bookshelf.appendChild(BookList);
+}
+
+function SaveBook(title, author) {
+  const book = new Book(title, author);
+  if (!Array.isArray(Books)) {
+    Books = [];
+  }
+  Books.push(book);
+  localStorage.Books = JSON.stringify(Books);
+  ReloadBooks();
+}
+
+function AddBook() {
+  const formAddBook = document.forms.AddBook;
+  const bookData = new FormData(formAddBook);
+  const bookTitle = bookData.get('title');
+  const bookAuthor = bookData.get('author');
+  formAddBook.reset();
+  SaveBook(bookTitle, bookAuthor);
+}
+
+submit.addEventListener('click', () => {
+  AddBook();
+});
+
+// Load the Library on opening the page
+ReloadBooks();
